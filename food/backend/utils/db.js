b6ef5e9715e176683bdde792b1e01aaadc4961db
ravan1010 <ravanravana177@gmail.com>
2025-08-19 21:35:50 +0530

    main

--- food/backend/utils/db.js
+++ food/backend/utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
-const URL = 'mongodb+srv://ravanravana177:c0RPxHEuxVPyxRgw@cluster0.5m8gkwy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const URL = 'mongodb+srv://ravanravana177:r1a09u4hx4u9sT4T@cluster0.5m8gkwy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const dbconnection = async () => {
 
