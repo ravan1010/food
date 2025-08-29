45c8bc0be72100c5b7d98bdf51b726a925127543
ravan1010 <ravanravana177@gmail.com>
2025-08-29 15:10:07 +0530

    main

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: 'https://food-4zgz.onrender.com',
-  baseURL: 'http://localhost:5001',
+  baseURL: 'https://food-4zgz.onrender.com',
+  // baseURL: 'http://localhost:5001',
   withCredentials: true,
 });
 
